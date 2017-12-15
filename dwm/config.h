abaa6185729d29cab784d4ce0e772bb5368ff1c1
Håvard Pettersson <mail@haavard.me>
2017-12-15 13:39:42 +0100

    urxvt: autostart urxvtd on demand

--- dwm/config.h
+++ dwm/config.h
@@ -51,7 +51,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "rofi", "-show", "combi", NULL };
-static const char *termcmd[]  = { "urxvtc", NULL };
+static const char *termcmd[]  = { "urxvtc-autostart", NULL };
 
 static const char *screenshotcmd[]    = { "scr", NULL };
 static const char *screenshotselcmd[] = { "scr", "--select", NULL };
